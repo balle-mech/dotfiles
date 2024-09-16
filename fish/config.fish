4c53f4a8d4bb64067528d81ad08e4359d525000c
a2c6201 <a2c6201@gmail.com>
2024-09-16 11:50:41 +0900

    ghqのキーバインド設定

---
+++ fish/config.fish
@@ -0,0 +1,17 @@
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /Users/fukunagaatsushi/anaconda3/bin/conda
+    eval /Users/fukunagaatsushi/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+end
+# <<< conda initialize <<<
+
+# ghqの管理化にあるリポジトリを一覧表示するキーバインドを設定
+function ghq-src
+    set selected_dir (ghq list -p | peco --query (commandline -b))
+    if test -n "$selected_dir"
+        cd $selected_dir
+    end
+end
+
+bind \cg ghq-src
