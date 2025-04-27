5506ec8f5c388a26be27922817db8ccc68e1c740
a2c6201 <a2c6201@gmail.com>
2025-04-27 13:52:07 +0900

    conda 削除

--- .bash_profile
+++ .bash_profile
@@ -17,9 +17,5 @@ else
     fi
 fi
 unset __conda_setup
-
-if [ -f "/Users/fukunagaatsushi/miniforge3/etc/profile.d/mamba.sh" ]; then
-    . "/Users/fukunagaatsushi/miniforge3/etc/profile.d/mamba.sh"
-fi
 # <<< conda initialize <<<
 
--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,5 @@
+set -x PATH /opt/homebrew/bin $PATH
+
 # ghqの管理化にあるリポジトリを一覧表示するキーバインドを設定
 function ghq-src
     set selected_dir (ghq list -p | peco --query (commandline -b))
@@ -7,3 +9,17 @@ function ghq-src
 end
 
 bind \cg ghq-src
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /Users/fukunagaatsushi/miniforge3/bin/conda
+    eval /Users/fukunagaatsushi/miniforge3/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/Users/fukunagaatsushi/miniforge3/etc/fish/conf.d/conda.fish"
+        . "/Users/fukunagaatsushi/miniforge3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/Users/fukunagaatsushi/miniforge3/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+
